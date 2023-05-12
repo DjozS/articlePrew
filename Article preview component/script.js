c94aaca13a040a7e2e29ff4af5aecd1ef135333e
Djoz <127934363+DjozS@users.noreply.github.com>
2023-05-12 17:17:36 +0200

    Add files via upload

---
+++ Article preview component/script.js
@@ -0,0 +1,6 @@
+const btnShare = document.querySelector(".btnShare")
+const share = document.querySelector(".share")
+btnShare.addEventListener("click", function(){
+    share.style.display = "flex";
+})
+
